36f891b701747a4d3dff89b88ec546ccddcc53d6
Mr.Hoang PC <hoangnph@gmail.com>
2020-04-21 22:25:03 +0700

    python read and control Excel. Next Step: Send email from Python

--- test.py
+++ test.py
@@ -5,15 +5,30 @@
 import pandas as pd
 import tkinter as tk
 from tkinter import filedialog
+import datetime
 
 root = tk.Tk()
 
-# canvas1 = tk.Canvas(root, width = 300, height = 300, bg = 'lightsteel')
-# Assign spreadsheet filename to `file`
-# r character is define draw String.
-file = r'PlanNow.xlsx'
+# Khai bao bien global
+date = datetime.date.today().strftime('%d/%m/%Y')
+file = r'DailyReport_STECH.xlsx'
 
 #Load spreadsheet
-data = pd.read_excel(file)
-df = pd.DataFrame(data, columns= ['Project Planner','Unnamed: 2'])
-print(df)
+xls= pd.ExcelFile(file)
+sheet = xls.sheet_names
+string = "Here is all sheet: {}".format(sheet)
+print(string)
+
+df = pd.DataFrame(pd.read_excel(file,0))
+#print(df)
+bodyContent=""
+title = "[STECH Báo cáo ngày] Báo cáo công việc ngày {}".format(date)
+
+
+for index,row in df.iterrows():
+    if row[0] == date:
+        #print(row[1],row[2],row[3])
+        bodyContent += "Task {} : \n {} \n \t Kết quả: \n \t \t {} \n \t Khó khăn: \n \t \t {} \n \t Đề xuất: \n \t \t {} \n \t Kế hoạch tiếp theo: \n \t \t {} \n ".format(row[1],row[2],row[3],row[4],row[5],row[6])
+
+print(title)
+print(bodyContent)
